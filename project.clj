3bff8b81ee906e6ea39ac2564e403c611666d6fa
Emil Bengtsson <emil@emil0r.com>
2015-04-21 21:59:47 +0200

    can set a base directory to check for files updated deps

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject ez-image "1.0.2"
+(defproject ez-image "1.0.3"
   :description "Lightweight image manipulation"
   :url "https://github.com/emil0r/ez-image"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [me.raynes/fs "1.4.5"]
-                 [digest "1.4.3"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [me.raynes/fs "1.4.6"]
+                 [digest "1.4.4"]
                  [org.imgscalr/imgscalr-lib "4.2"]])
--- src/ez_image/core.clj
+++ src/ez_image/core.clj
@@ -1,24 +1,47 @@
 (ns ez-image.core
   (:require [clojure.java.io :as io]
-            [clojure.string :as s]
+            [clojure.string :as str]
+            [digest :refer [md5]]
             [ez-image.method :as method]
             [ez-image.mode :as mode]
             [ez-image.rotation :as rotation]
             [me.raynes.fs :as fs])
-  (:use [digest :only [md5]])
   (:import [java.io File]
            [java.awt Color]
            [javax.imageio ImageIO]
            [org.imgscalr Scalr]))
 
+(def sep "/")
 
-(def ^:private -cache (atom {}))
-(defn setup! [save-path web-path]
-  (swap! -cache assoc :save-path save-path)
-  (swap! -cache assoc :web-path web-path))
+(defonce ^:private -cache (atom {}))
+(defonce ^:private -options (atom {}))
+(defn setup!
+  ([{:keys [save-path web-path base-dir sep] :as options}]
+     (swap! -options assoc :save-path save-path)
+     (swap! -options assoc :web-path web-path)
+     (swap! -options assoc :base-dir base-dir)
+     (if sep
+       (alter-var-root #'sep (fn [old-value] sep))))
+  ([save-path web-path]
+     ^{:deprecated "Use the options arity instead"}
+     (swap! -options assoc :save-path save-path)
+     (swap! -options assoc :web-path web-path)))
+
+(defn- get-path [filename]
+  (if-let [base-dir (:base-dir @-options)]
+    (if (fs/exists? filename)
+      filename
+      (let [sep-pattern (re-pattern sep)
+            new-filename (->> (concat (str/split base-dir sep-pattern)
+                                      (str/split filename sep-pattern))
+                              (remove str/blank?)
+                              (str/join sep)
+                              (str sep))]
+        new-filename))
+    filename))
 
 (defn- extension [path]
-  (last (s/split path #"\.")))
+  (last (str/split path #"\.")))
 (defn- image-io [path]
   (-> path io/as-file ImageIO/read))
 (defn save! [image path]
@@ -32,7 +55,7 @@
 
 (defn delete!
   ([]
-     (if-let [save-path (:save-path @-cache)]
+     (if-let [save-path (:save-path @-options)]
        (do
          (fs/delete-dir save-path)
          (fs/mkdir save-path)
@@ -44,8 +67,8 @@
          (clear! filename)))))
 
 (defn- add-to-cache [digested-filename]
-  (let [to-cache {:save-path (str (:save-path @-cache) digested-filename)
-                  :web-path (str (:web-path @-cache) digested-filename)}]
+  (let [to-cache {:save-path (str (:save-path @-options) digested-filename)
+                  :web-path (str (:web-path @-options) digested-filename)}]
     (swap! -cache assoc digested-filename to-cache)
     to-cache))
 
@@ -54,8 +77,8 @@
 
 (defn- try-cache-values []
   (cond
-   (nil? (get @-cache :save-path)) (throw (Exception. ":save-path not set"))
-   (nil? (get @-cache :web-path)) (throw (Exception. ":web-path not set"))
+   (nil? (get @-options :save-path)) (throw (Exception. ":save-path not set"))
+   (nil? (get @-options :web-path)) (throw (Exception. ":web-path not set"))
    :else true))
 
 (defn- constrain
@@ -129,21 +152,23 @@
   img)
 
 (defn convert [filename & commands]
-  (loop [img (image-io filename)
+  (loop [img (image-io (get-path filename))
          [c & commands] commands]
     (if (nil? c)
       img
       (recur (command img c) commands))))
 
 (defn cache [filename & commands]
-  (if (try-cache-values)
-    (let [digested-filename (generate-filename filename commands)]
-      (if-let [cached (get @-cache digested-filename)]
-        (:web-path cached)
-        (if (fs/exists? (str (:save-path @-cache) digested-filename))
-          (:web-path (add-to-cache digested-filename))
-          (if-let [img (apply convert filename commands)]
-            (do
-              (save! img (str (:save-path @-cache) digested-filename))
-              (:web-path (add-to-cache digested-filename)))))))))
+  ;; see if things have been initialized
+  (try-cache-values)
 
+  ;; get our cached image (or generate it)
+  (let [digested-filename (generate-filename filename commands)]
+    (if-let [cached (get @-cache digested-filename)]
+      (:web-path cached)
+      (if (fs/exists? (str (:save-path @-options) digested-filename))
+        (:web-path (add-to-cache digested-filename))
+        (if-let [img (apply convert filename commands)]
+          (do
+            (save! img (str (:save-path @-options) digested-filename))
+            (:web-path (add-to-cache digested-filename))))))))
